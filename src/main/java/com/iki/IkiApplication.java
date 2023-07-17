3bfa590c326db3082084b7010e60d9f769d9a6c1
Egenieee <hhgg0925@naver.com>
2023-07-17 13:14:37 +0900

    build: 초기 세팅

---
+++ src/main/java/com/iki/IkiApplication.java
@@ -0,0 +1,13 @@
+package com.iki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IkiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iki/IkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
