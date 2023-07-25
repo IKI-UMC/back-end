91eea4e122b9e33b81ad9238046a2b483759550c
Egenieee <hhgg0925@naver.com>
2023-07-25 18:11:43 +0900

    feat: 장바구니 총 가격 변수 추가

--- src/main/java/com/iki/domain/dto/Cart/CartResponseDto.java
+++ src/main/java/com/iki/domain/dto/Cart/CartResponseDto.java
@@ -10,11 +10,15 @@ import java.util.List;
 @Getter
 public class CartResponseDto {
     Long orderUsersId;
+    int totalPrice = 0;
     List<OrderMenuResponseDto> orderMenuResponseDtoList;
 
     @Builder
     public CartResponseDto(List<OrderMenuResponseDto> orderMenuResponseDtoList, OrderUsers users) {
         this.orderUsersId = users.getOrderUsersId();
         this.orderMenuResponseDtoList = orderMenuResponseDtoList;
+        for (OrderMenuResponseDto responseDto : orderMenuResponseDtoList) {
+            totalPrice += responseDto.getOrderMenuPrice();
+        }
     }
 }
