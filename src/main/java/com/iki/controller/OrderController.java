7783d01d24ba63ecb0ae94ecb93e34ba4c8510ee
KoungQ <guu119@naver.com>
2023-07-31 21:07:43 +0900

    feat: Order(주문) 기능 및 API 생성

---
+++ src/main/java/com/iki/controller/OrderController.java
@@ -0,0 +1,41 @@
+package com.iki.controller;
+
+import com.iki.config.ResponseApiMessage;
+import com.iki.domain.dto.Order.OrderResponseDto;
+import com.iki.domain.dto.Order.OrderSaveRequestDto;
+import com.iki.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/api/v1/orders")
+@RequiredArgsConstructor
+@RestController
+public class OrderController extends BaseController {
+    private final static int SUCCESS_CODE = 200;
+    private final OrderService orderService;
+
+    @ResponseBody
+    @PostMapping("")
+    public ResponseEntity<ResponseApiMessage> save(@RequestBody OrderSaveRequestDto requestDto) {
+        Long orderId = orderService.save(requestDto);
+
+        return sendResponseHttpByJson(SUCCESS_CODE, "Saved Order=" + orderId, orderId);
+    }
+
+    @ResponseBody
+    @GetMapping("/{orderId}")
+    public ResponseEntity<ResponseApiMessage> findById(@PathVariable Long orderId) {
+        OrderResponseDto responseDto = orderService.findById(orderId);
+
+        return sendResponseHttpByJson(SUCCESS_CODE, "Order loaded. ORDER_ID=" + orderId, responseDto);
+    }
+
+//    @ResponseBody
+//    @DeleteMapping("/{orderId}")
+//    public ResponseEntity<ResponseApiMessage> delete(@PathVariable Long orderId) {
+//        Long deletedOrderId = orderService.delete(orderId);
+//
+//        return sendResponseHttpByJson(SUCCESS_CODE, "ORDER DELETED. ORDER_ID=" + orderId, deletedOrderId);
+//    }
+}
